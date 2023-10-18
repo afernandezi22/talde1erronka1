2a58a06f9e2a4cd0aa588563986dc6f5b6b6da1f
afernandezi22 <afernandezi22@fpsanjorge.com>
2023-10-18 09:03:36 +0200

    Controller, db eta defaultdb klaseak komentatu.

--- repository/db.php
+++ repository/db.php
@@ -1,5 +1,6 @@
 <?php
     include "defaultdb.php";
+    //Datubaserako konexioak sortzkeo klasea
     class DB implements DefaultDB{
         private $servername;
         private $username;
@@ -7,6 +8,7 @@
         private $database;
         private $conn;
 
+        //Sortzailea
         public function __construct(){
             $this -> servername = "localhost";
             $this -> username = "root";
@@ -14,6 +16,7 @@
             $this -> database = "3wag2e1";
         }
 
+        //Datubasera konektatzeko funtzioa
         private function connect(){
             $this -> conn = new mysqli($this -> servername, $this -> username, $this -> password, $this -> database);
             
@@ -25,24 +28,33 @@
             return $this -> conn;
         }
 
+        //SELECT egiteko erabiltzen den funtzioa
         public function select($sql){
+            //Konexioa sortu
             $this -> connect();
+            //Emaitza lortu kontsulta eginez
             $emaitza = $this -> conn -> query($sql);
+            //0 baino gehiagoko erantzuna ematen badu emaitza bueltatu, bestela 0 bueltatu
             if ($emaitza -> num_rows > 0){
                 return $emaitza;
             }else{
                 return 0;
             }
+            //Konexioa itxi
             $this -> conn -> close();
         }
 
+        //INSERT, UPDATE eta DELETE egiteko erabiltzen den funtzioa
         public function do($sql){
+            //Konexioa sortu
             $this -> connect();
+            //Sententzia ondo exekutatzen bada emaitza bueltatu, bestela 0 bueltatu
             if ($emaitza = $this -> conn -> query($sql)){
                 return $emaitza;
             }else{
                 return 0;
             }
+            //Konexioa itxi
             $this -> conn -> close();
         }
     }
